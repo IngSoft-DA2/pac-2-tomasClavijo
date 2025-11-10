4e65d289082ad617712ff7dbd841e5f970e501f3
Tomás <89207931+tomasClavijo@users.noreply.github.com>
2025-11-10 20:13:30 -0300

    fix: adding delay for state

--- Front/src/app/reflection/reflection.api.service.ts
+++ Front/src/app/reflection/reflection.api.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class ReflectionApiService {
   constructor(private http: HttpClient) {}
 
   getImporters(): Observable<string[]> {
-    return this.http.get<string[]>(`/api/reflection/importers`);
+    return this.http
+      .get<string[]>(`/api/reflection/importers`)
+      .pipe(delay(700));
   }
 }
-
